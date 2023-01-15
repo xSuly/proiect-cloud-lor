b71e13f9177c0780328032900e13053241a567c3
xSuly <81815152+xSuly@users.noreply.github.com>
2023-01-15 23:54:36 +0200

    Update email_server.py

--- var/www/uploads/email_server.py
+++ var/www/uploads/email_server.py
@@ -4,7 +4,7 @@ client = EmailClient.from_connection_string(connection_string);
 
 content = EmailContent(
         subject="Felicitari! Ati incarcat un videoclip!",
-        plain_text="Pentru a ridica castigul, va rugam sa il contactati pe radu.codreanu@s.unibuc.ro",
+        plain_text="Videoclip succes!",
         html= "<html><h1>Radu Stefan Codreanul va va asista daca nu reusiti sa accesati thumbnail-ul liviu.albei@s.unibuc.ro/cojocaru.andrei.3@s.unibuc.ro/radu.codreanu@s.unibuc.ro!</h1></html>",
 )
 
