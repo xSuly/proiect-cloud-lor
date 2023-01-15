15b90ddd8ae0027fc53507fe9a6735e1adcfcee1
joyboy <81815162+radustefan2311@users.noreply.github.com>
2023-01-15 23:47:27 +0200

    Stergere mesaj din azure

--- var/www/uploads/dequeue_service.py
+++ var/www/uploads/dequeue_service.py
@@ -16,10 +16,5 @@ messages = queue_client.receive_messages()
 for message in messages:
     message_content = message.content
 
-    # Daca ati adaugat mesaje din browser, implicit este selectata
-    # optiunea "Encode the message body in Base64". Pentru decodare
-    # folositi linia de mai jos
-    # message_content = b64decode(message.content).decode("utf-8")
-
     print("Dequeueing message: " + message_content)
     queue_client.delete_message(message.id, message.pop_receipt)
